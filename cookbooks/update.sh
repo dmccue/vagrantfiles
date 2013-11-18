21de69257aae6fadda278217b8d65a471b40124b
damccue <damccue@git.com>
2013-11-18 09:56:08 +0000

    added cookbook update script

---
+++ cookbooks/update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git clone https://github.com/opscode-cookbooks/logrotate.git
+git clone https://github.com/opscode-cookbooks/cron.git
+git clone https://github.com/opscode-cookbooks/chef-server.git
+git clone https://github.com/opscode-cookbooks/chef-client.git
+git clone https://github.com/opscode-cookbooks/ntp.git
+
